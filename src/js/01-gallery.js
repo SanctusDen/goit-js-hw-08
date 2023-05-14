056212784a11d652d9a7b1cd0b3e48dfe2f35927
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-05-14 15:54:36 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryAll = document.querySelector(".gallery");
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+galleryAll.insertAdjacentHTML("afterbegin", galleryEl);
+
+new SimpleLightbox(".gallery__link", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
