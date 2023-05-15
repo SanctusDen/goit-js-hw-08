57179ba5d9d99dc3404f2ab1fa82a41fcdfed7b0
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-05-15 22:22:45 +0300

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', throttle(onSaveInput, 500));
+
+textareaPopulate();
+function onSubmitForm(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state", objMessage); 
+};
+
+function onSaveInput(evt) {
+    message.value = evt.currentTarget.value;
+    email.value = evt.currentTarget.value;
+
+    const objMessage = { email, message };
+    localStorage.setItem("feedback-form-state", objMessage);
+};
+
+function textareaPopulate(evt) {
+    const savedMessage = localStorage.getItem("feedback-form-state");
+    if (savedMessage) {
+        textarea.value = savedMessage; 
+    }
+};
